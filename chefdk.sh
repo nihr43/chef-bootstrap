394b24a88ce6eb33268f50b9a6c4041f75d6a8f2
nh <you@example.com>
2018-11-01 10:49:17 -0500

    key names

--- chefdk.sh
+++ chefdk.sh
@@ -6,18 +6,18 @@ yum install wget ruby git -y
 wget https://packages.chef.io/files/stable/chefdk/3.3.23/el/7/chefdk-3.3.23-1.el7.x86_64.rpm
 rpm -Uhv ./chefdk-3.3.23-1.el7.x86_64.rpm
 
-chef generate repo chef-repo
-mkdir -p ~/chef-repo/.chef
+chef generate repo /root/repo
+mkdir /root/repo/.chef
 
-cat > ~/chef-repo/.chef/knife.rb << EOF_KNIFE
+cat > /root/repo/.chef/knife.rb << EOF_KNIFE
 current_dir = File.dirname(__FILE__)
 log_level                 :info
 log_location              STDOUT
 node_name                 "testuser"
-client_key                "#{current_dir}/chef_key.pem"
+client_key                "#{current_dir}/chefuser.pem"
 chef_server_url           "https://chef-master.localdomain/organizations/org"
 cookbook_path             ["#{current_dir}/../cookbooks"]
 EOF_KNIFE
 
 
-echo "/n/nchefdk setup nearly finished.  you need to set your username, key, and chef server url"
+echo "chefdk setup nearly finished.  you need to set your username, key, and chef server url"
